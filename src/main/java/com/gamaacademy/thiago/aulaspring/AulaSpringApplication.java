6f53de47da67e26c1cf5a5054ad319e67cc5e3fb
Thiago Felipe <thiagofg.eng@gmail.com>
2022-04-15 19:23:14 -0300

    Criação da classe/entidade Usuario e configuração da conexão com o banco mySql

---
+++ src/main/java/com/gamaacademy/thiago/aulaspring/AulaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gamaacademy.thiago.aulaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamaacademy/thiago/aulaspring/AulaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamaacademy.thiago.aulaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
